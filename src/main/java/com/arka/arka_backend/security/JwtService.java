78266e28f4d4cfe007b30628095296cf119e0fec
Zeeshan Khan <zeeshankhan@Zeeshans-MacBook-Air.local>
2026-02-26 04:23:13 +0530

    Fix: Properly permit admin login and register paths.v2

--- src/main/java/com/arka/arka_backend/security/JwtService.java
+++ src/main/java/com/arka/arka_backend/security/JwtService.java
@@ -3,42 +3,34 @@ package com.arka.arka_backend.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-
 import io.jsonwebtoken.security.Keys;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
-
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Service
 public class JwtService {
 
-    private final String SECRET =
-            "arkaSuperSecretKeyarkaSuperSecretKey123456";
+    @Value("${jwt.secret}")
+    private String secretKey;
 
     private SecretKey getSignKey() {
-
-        return Keys.hmacShaKeyFor(SECRET.getBytes());
-
+        return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
     }
 
     public String generateToken(String email) {
-
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date())
-                .setExpiration(
-                        new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)
-                )
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
                 .signWith(getSignKey(), SignatureAlgorithm.HS256)
                 .compact();
-
     }
 
     public String extractEmail(String token) {
-
         Claims claims = Jwts.parserBuilder()
                 .setSigningKey(getSignKey())
                 .build()
@@ -46,7 +38,5 @@ public class JwtService {
                 .getBody();
 
         return claims.getSubject();
-
     }
-
 }
