541ad09794bddd75d41647d8b879a85a53243d10
Zeeshan Khan <zeeshankhan@Zeeshans-MacBook-Air.local>
2026-02-25 08:16:42 +0530

    still trying to fix port issue

--- src/main/java/com/arka/arka_backend/service/S3Service.java
+++ src/main/java/com/arka/arka_backend/service/S3Service.java
@@ -1,16 +1,16 @@
 package com.arka.arka_backend.service;
+
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.core.sync.RequestBody;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-
-import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 
 import java.io.IOException;
 import java.util.UUID;
@@ -31,67 +31,36 @@ public class S3Service {
             @Value("${aws.secret.key}") String secretKey,
             @Value("${aws.region}") String region
     ) {
-
-        AwsBasicCredentials credentials =
-                AwsBasicCredentials.create(accessKey, secretKey);
-
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
         this.s3Client = S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(
-                        StaticCredentialsProvider.create(credentials))
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
                 .build();
     }
 
-
-    // Upload
     public String uploadFile(MultipartFile file) throws IOException {
+        String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .contentType(file.getContentType())
+                .build();
 
-        String fileName =
-                UUID.randomUUID() + "_" + file.getOriginalFilename();
+        s3Client.putObject(putObjectRequest, RequestBody.fromBytes(file.getBytes()));
 
-        PutObjectRequest putObjectRequest =
-                PutObjectRequest.builder()
-                        .bucket(bucketName)
-                        .key(fileName)
-                        .contentType(file.getContentType())
-                        .build();
-
-        s3Client.putObject(
-                putObjectRequest,
-                software.amazon.awssdk.core.sync.RequestBody
-                        .fromBytes(file.getBytes())
-        );
-
-        return "https://" + bucketName +
-                ".s3." + region +
-                ".amazonaws.com/" + fileName;
+        return "https://" + bucketName + ".s3." + region + ".amazonaws.com/" + fileName;
     }
 
-
-    // DELETE FROM S3
     public void deleteFile(String imageUrl) {
-
         try {
-
-            String fileName =
-                    imageUrl.substring(imageUrl.lastIndexOf("/") + 1);
-
-            DeleteObjectRequest deleteObjectRequest =
-                    DeleteObjectRequest.builder()
-                            .bucket(bucketName)
-                            .key(fileName)
-                            .build();
-
+            String fileName = imageUrl.substring(imageUrl.lastIndexOf("/") + 1);
+            DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(fileName)
+                    .build();
             s3Client.deleteObject(deleteObjectRequest);
-
-            System.out.println("Deleted from S3: " + fileName);
-
         } catch (Exception e) {
-
-            System.out.println("S3 delete failed");
             e.printStackTrace();
-
         }
     }
-
 }
